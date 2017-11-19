8464a6f5b5f1952e961c06950c545a676906027e
LYU KUN YU <davidlu0629@users.noreply.github.com>
2017-11-19 15:26:49 +0800

    Update readandwritefile.py

--- readandwritefile.py
+++ readandwritefile.py
@@ -3,4 +3,17 @@ import pandas as pd
 import csv 
 import math
 
+#read file
+##file address
+query_table="/home/ryuu/ir/hw1/query_list.txt"
+##read file from row 1
+with open(query_table, 'r') as doc:
+	reader= csv.reader(doc)
+	qlist= [[str(s) for s in row] for i, row in enumerate(reader) if i >=0]
+  
 
+#open new file and write
+##open
+file= open("/home/ryuu/ir/hw1/"+"submission.txt",'a')
+##write
+file.write("Query,RetrievedDocuments\n")
